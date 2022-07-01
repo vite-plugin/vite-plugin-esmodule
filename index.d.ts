844a8863955f34b45fd34329f726a199b52427ba
草鞋没号 <308487730@qq.com>
2022-07-01 15:12:55 +0800

    refactor(🚨): remove Vite build mode

--- index.d.ts
+++ index.d.ts
@@ -4,21 +4,13 @@ import type { Configuration } from 'webpack';
 export default esmodule;
 declare const esmodule: Esmodule;
 
-export interface WebpackOptions {
-  webpack?: true
-    | ((config: Configuration) => Configuration | void | Promise<Configuration | void>);
-  vite?: never;
-}
-
-export interface ViteOptions {
-  vite?: true
-    | ((config: UserConfig) => UserConfig | void | Promise<UserConfig | void>);
-  webpack?: never;
-}
-
 export interface Esmodule {
   (
     modules: (string | { [module: string]: string })[],
-    options?: WebpackOptions | ViteOptions,
+    options?: {
+      webpack?:
+      | Configuration
+      | ((config: Configuration) => Configuration | void | Promise<Configuration | void>);
+    },
   ): Plugin[];
 }
