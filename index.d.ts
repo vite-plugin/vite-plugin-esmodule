9ca00dcc922583055db683755406c0b40e066a7c
草鞋没号 <308487730@qq.com>
2022-07-26 14:02:51 +0800

    refactor: better type define

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,11 @@
-import type { Plugin } from 'vite';
-import type { Configuration } from 'webpack';
+import type { Plugin } from 'vite'
+import type { Configuration } from 'webpack'
 
-export default esmodule;
-declare const esmodule: Esmodule;
+export type ModuleRecord = string | { [module: string]: string }
 
-export interface Esmodule {
-  (
-    /**
-     * If modules are not passed in, ESM packages will be automatically obtained from package.json in process.cwd path
-     */
-    modules?: (string | { [module: string]: string })[],
-    webpack?: ((config: Configuration) => Configuration | void | Promise<Configuration | void>),
-  ): Plugin;
-}
+declare function exports(
+  modules: ModuleRecord[] | ((esmPkgs: string[]) => ModuleRecord[]),
+  webpack?: ((config: Configuration) => Configuration | void | Promise<Configuration | void>),
+): Plugin
+
+export = exports
