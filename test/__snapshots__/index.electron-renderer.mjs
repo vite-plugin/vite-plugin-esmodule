3eeb30d9b729e5e7ce33995794c58690f68a8fc3
草鞋没号 <308487730@qq.com>
2022-10-19 14:01:00 +0800

    refactor(test): better test

--- test/__snapshots__/index.electron-renderer.mjs
+++ test/__snapshots__/index.electron-renderer.mjs
@@ -1,6 +1,6 @@
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
-const _M_ = require("./index.js");
+const _M_ = require(".vite-plugin-esmodule/file-type");
 export const fileTypeFromBuffer = _M_.fileTypeFromBuffer;
 export const fileTypeFromFile = _M_.fileTypeFromFile;
 export const fileTypeFromStream = _M_.fileTypeFromStream;
@@ -8,7 +8,7 @@ export const fileTypeFromTokenizer = _M_.fileTypeFromTokenizer;
 export const fileTypeStream = _M_.fileTypeStream;
 export const supportedExtensions = _M_.supportedExtensions;
 export const supportedMimeTypes = _M_.supportedMimeTypes;
-export const keyword_default = _M_.default || _M_;
+const keyword_default = _M_.default || _M_;
 export {
   keyword_default as default,
 };
--- test/index.js
+++ test/index.js
@@ -3,17 +3,16 @@ process.env.NODE_ENV = 'test-mjs';
 const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
+const assert = require('assert').strict;
 const vite = require('vite');
 const esmodule = require('vite-plugin-esmodule');
 const test = esmodule.__test__;
-const modules = ['file-type'];
+const moduleName = 'file-type';
+const node_modules = path.join(__dirname, '../node_modules');
+const __snapshots__ = path.join(__dirname, '__snapshots__');
 
-function __snapshots__id(id) {
-  return {
-    id: `./__snapshots__/${id}/index.js`,
-    electronId: `./__snapshots__/${id}/index.electron-renderer.mjs`,
-  };
-}
+fs.rmSync(__snapshots__, { recursive: true, force: true });
+fs.mkdirSync(__snapshots__, { recursive: true });
 
 ; (async () => {
   await vite.build({
@@ -28,67 +27,42 @@ function __snapshots__id(id) {
         fileName: format => format === 'es' ? '[name].mjs' : '[name].cjs',
       },
       rollupOptions: {
-        external: source => source.includes('__snapshots__'),
+        external: /__snapshots__/,
       },
     },
     plugins: [
-      esmodule(modules),
-      {
-        name: 'rewrite-resolveId',
-        enforce: 'pre',
-        resolveId(source) {
-          if (source.includes(test.CACHE_DIR)) {
-            // /Absolute-path/node_modules/.vite-plugin-esmodule/file-type/index.js
-            const [, relative] = source.split(test.CACHE_DIR);
-            // file-type
-            const name = path.dirname(relative).slice(1);
-            const { id, electronId } = __snapshots__id(name);
-            return electronId;
-          }
-        },
-      },
+      esmodule([moduleName]),
     ],
+    resolve: {
+      alias: { 'file-type': './__snapshots__/index.electron-renderer.mjs' },
+    },
   });
   preTest();
   runTest();
 })();
 
 function preTest() {
-  const __snapshots__ = path.join(__dirname, '__snapshots__');
-  const node_modules = path.join(__dirname, '../node_modules');
   const destdir = path.join(node_modules, test.CACHE_DIR);
-  fs.rmSync(__snapshots__, { recursive: true, force: true });
-
   /**
    * @type {(options: import('vite-plugin-optimizer').OptimizerArgs, ...args: Parameters<import('vite-plugin-esmodule')>) => void}
    */
   const writeElectronRendererServeESM = test.writeElectronRendererServeESM;
-  writeElectronRendererServeESM({ dir: destdir }, modules);
+  writeElectronRendererServeESM({ dir: destdir }, [moduleName]);
 
-  for (const mod of modules) {
-    const { cjsId, electronRendererId } = test.getModuleId(path.join(destdir, mod));
-    const testCjsId = path.join(__snapshots__, cjsId.replace(destdir, ''));
-    const testElectronRendererId = path.join(__snapshots__, electronRendererId.replace(destdir, ''));
-    test.ensureDir(testCjsId);
-    test.ensureDir(testElectronRendererId);
-    fs.writeFileSync(testCjsId, fs.readFileSync(cjsId, 'utf8'));
-    fs.writeFileSync(
-      testElectronRendererId.replace('.js', '.mjs'),
-      'import { createRequire } from "node:module";\nconst require = createRequire(import.meta.url);\n' +
-      fs.readFileSync(electronRendererId, 'utf8'),
-    );
-  }
+  const { electronRendererId } = test.getModuleId('');
+  fs.writeFileSync(
+    path.join(__snapshots__, electronRendererId.replace('.js', '.mjs')),
+    `import { createRequire } from "node:module";\nconst require = createRequire(import.meta.url);\n`
+    +
+    fs.readFileSync(path.join(node_modules, test.CACHE_DIR, moduleName, electronRendererId), 'utf8'),
+  );
 }
 
 function runTest() {
   const child = cp.spawn('node', ['./-.mjs'], { cwd: __dirname });
   child.stdout.on('data', chunk => {
     const str = chunk.toString().trim();
-    console.log('[test]', str);
-    if (str === '[AsyncFunction: fileTypeFromFile]') {
-      console.log('[test] success.');
-    } else {
-      throw new Error('[test] field!');
-    }
+    assert.equal(str, '[AsyncFunction: fileTypeFromFile]');
+    console.log('test success');
   });
 }
